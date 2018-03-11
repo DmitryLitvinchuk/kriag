17ad77decc95a3b23bee8ac93de8325a2fe91394
DmitryLitvinchuk <colororiginal66@gmail.com>
2018-03-12 02:23:37 +0300

    Profile

---
+++ blog/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from .models import Post, User, Profile
+
+class PostForm(forms.ModelForm):
+
+    class Meta:
+        model = Post
+        fields = ('title', 'text',)
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email')
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('name', 'gender', 'birth_date', 'info')
--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^accounts/register/$', views.RegisterFormView.as_view()),
+    url(r'^accounts/login/$', views.LoginFormView.as_view()),
+    url(r'^accounts/logout/$', views.LogoutView.as_view()),
+    url(r'^accounts/profile/$', views.update_profile, name='update_profile'),
 ]
